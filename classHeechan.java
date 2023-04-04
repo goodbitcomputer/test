7b6d63169c29d4ed7905f38d645e0045577c41b2
82105 <change_1997@naver.com>
2023-04-04 12:28:03 +0900

    new branch

--- classHeechan.java
+++ classHeechan.java
@@ -1,5 +1,7 @@
 public class classHeechan {
     public static void main(String args[]){
         System.out.println("it's test.");
+        System.out.println("and new branch created.");
     }
+
 }
